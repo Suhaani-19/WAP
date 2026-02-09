a5eaccc96528af1275d318218b4f4a8c719810c3
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-02-09 20:57:48 +0530

    feat: add interactive visuals and animations

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+
+// *writing file sync*
+
+// console.log("Hi")
+// const data=fs.writeFileSync("note.txt","Hello,I'm learning FS")
+// console.log(data)
+// console.log("123")
+
+// *reading file sync*
+const result = fs.readFileSync("note.txt","utf-8")
+console.log(result , typeof result)   
+fs.writeFileSync("note.txt","Hello,I'm learning FS")
+fs.appendFileSync("note.txt"," \nHello,I'm learning ")
+
+
+fs.writeFileSync("note123.txt"," \nHello,I'm learning FS",{flag:"a"}) // first write this only and run
+
+
+
+// fs.unlinkSync("note123.txt") //then write this
