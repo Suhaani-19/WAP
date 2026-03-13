7dcdb7e9726adf98244ff74258823100d16182cb
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-03-13 18:37:12 +0530

    comments

--- asyncFile.js
+++ asyncFile.js
@@ -2,7 +2,7 @@ const fs= require("fs")
 console.log("Start")
 fs.readFile("note.txt","utf-8",(error,data)=>{console.log("File read",data)}
 )
-
+// writefile
 fs.writeFile("note123.txt","Writingggg",()=>{console.log("File write")})
 console.log("End")
 
