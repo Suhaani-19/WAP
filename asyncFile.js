2f96bd5f88ca19d0d6703c44f66a66844a806fa8
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-03-17 17:59:38 +0530

    improved

--- asyncFile.js
+++ asyncFile.js
@@ -1,10 +1,45 @@
-const fs= require("fs")
-console.log("Start")
-fs.readFile("note.txt","utf-8",(error,data)=>{console.log("File read",data)}
-)
-// writefile
-fs.writeFile("note123.txt","Writingggg",()=>{console.log("File write")})
-console.log("End")
+const fs = require("fs");
 
+console.log("Start");
 
+// read file
+fs.readFile("note.txt", "utf-8", (error, data) => {
+    if (error) {
+        console.log("Error reading file:", error);
+        return;
+    }
+    console.log("File read:", data);
+});
 
+// write file
+fs.writeFile("note123.txt", "Writingggg\n", (err) => {
+    if (err) {
+        console.log("Error writing file:", err);
+        return;
+    }
+
+    console.log("File write");
+
+    // append more text
+    fs.appendFile("note123.txt", "Adding more content\n", (err) => {
+        if (err) {
+            console.log("Error appending:", err);
+            return;
+        }
+
+        console.log("Content appended");
+
+        // read the newly written file
+        fs.readFile("note123.txt", "utf-8", (err, data) => {
+            if (err) {
+                console.log("Error reading new file:", err);
+                return;
+            }
+
+            console.log("Updated file content:");
+            console.log(data);
+        });
+    });
+});
+
+console.log("End");
