022b002771753442b87577a6a42d4467c4976c1b
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-03-25 20:06:42 +0530

    Added file stats, delete operation and improved FS workflow

--- asyncFile.js
+++ asyncFile.js
@@ -18,7 +18,7 @@ fs.writeFile("note123.txt", "Writingggg\n", (err) => {
         return;
     }
 
-    console.log("File write");
+    console.log("File written");
 
     // append more text
     fs.appendFile("note123.txt", "Adding more content\n", (err) => {
@@ -29,18 +29,39 @@ fs.writeFile("note123.txt", "Writingggg\n", (err) => {
 
         console.log("Content appended");
 
-        // read the newly written file
-        fs.readFile("note123.txt", "utf-8", (err, data) => {
+        // get file stats
+        fs.stat("note123.txt", (err, stats) => {
             if (err) {
-                console.log("Error reading new file:", err);
+                console.log("Error getting stats:", err);
                 return;
             }
 
-            console.log("Updated file content:");
-            console.log(data);
+            console.log("File Size:", stats.size, "bytes");
+            console.log("Created At:", stats.birthtime);
+
+            // read updated file
+            fs.readFile("note123.txt", "utf-8", (err, data) => {
+                if (err) {
+                    console.log("Error reading new file:", err);
+                    return;
+                }
+
+                console.log("Updated file content:");
+                console.log(data);
+
+                // delete file (cleanup)
+                fs.unlink("note123.txt", (err) => {
+                    if (err) {
+                        console.log("Error deleting file:", err);
+                        return;
+                    }
+
+                    console.log("File deleted successfully");
+                });
+            });
         });
     });
 });
-console.log("hi")
-console.log("hello")
-;
+
+console.log("hi");
+console.log("hello");
