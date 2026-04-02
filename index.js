0e8fa631222f8e228eeb43db163fa230456c3e2e
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-04-02 18:19:23 +0530

    code

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const fs = require("fs")
 
 // *writing file sync*
 
-// console.log("Hi")
-// const data=fs.writeFileSync("note.txt","Hello,I'm learning FS")
-// console.log(data)
-// console.log("123")
+console.log("Hi")
+const data=fs.writeFileSync("note.txt","Hello,I'm learning FS")
+console.log(data)
+console.log("123")
 
 // *reading file sync*
 const result = fs.readFileSync("note.txt","utf-8")
